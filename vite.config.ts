92eeaa4c77c9f8e5a912d1f25109e987c7b82770
Divyanshu <divyanshuvishwakarma222@gmail.com>
2024-11-28 13:20:29 +0530

    Fix : Fix Bug

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@
     
       export default defineConfig({
         plugins: [react()],
+        base: './',
         resolve: {
           alias: {
             "@": path.resolve(__dirname, "./src"),
